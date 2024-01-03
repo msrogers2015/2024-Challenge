0ae7bceb2b48e3e246e9496fd6862c11d2039cc0
Marquel Rogers <69293041+msrogers2015@users.noreply.github.com>
2024-01-03 08:38:21 -0600

    Player hand can be read and given to player.
    Player hand value can be calculated with the get_value function. This accounts for aces
    that push the player value over 21 if counted as 11 and adjust them to 1 as needed.

--- January/blackjack/main.py
+++ January/blackjack/main.py
@@ -1,8 +1,7 @@
 from random import shuffle, randint
-import time
 
 class Deck:
-    def __init__(self):
+    def __init__(self) -> None:
         '''Create information to build a deck of cards.'''
         # Create suit denominations
         self.suits = {
@@ -14,7 +13,7 @@ class Deck:
         # Create values
         self.values = ['A',1,2,3,4,5,6,7,8,9,10,'J','Q','K']
 
-    def create_deck(self):
+    def create_deck(self) -> None:
         '''Create a deck of cards'''
         # Create empty deck of cards
         self.deck = []
@@ -23,23 +22,64 @@ class Deck:
             for value in self.values:
                 self.deck.append(f'{value}-{suit}')
     
-    def shuffle_deck(self):
+    def shuffle_deck(self) -> None:
         '''Shuffle cards based on random seed using epoch time.'''
         # Shuffle deck 3-10 times
         for x in range(randint(3, 10)):
             shuffle(self.deck)
 
-    def deal_hand(self):
+    def deal_hand(self) -> list:
         '''Deal initial cards to start round.'''
+        pass
+
+    def deal_card(self) -> str:
+        '''Deal a single card if player chose hit.'''
+        pass
+
+class Player:
+    def __init__(self) -> None:
+        '''Create instance of a player.'''
+        # Create starting balance to bet with
+        self.bank = 500
+        # Create a list to hold current cards in hand.
+        self.hand = []
+        self.hand2 = []
+
+    def get_value(self, hand) -> int:
+        '''Add value of cards in players hand'''
+        # Create a  value varible to total card values in player hand.
+        hand_value = 0
+        # Loop through player hand to get values or convert face cards and ace
+        # cards to numerical value.
+        for card in hand:
+            card_value = card.split('-')[0]
+            if card_value.isdigit():
+                hand_value += int(card_value)
+            elif card_value in ['J','Q','K']:
+                hand_value += 10
+            else:
+                hand_value += 11
+        # Check if player hand value is over 21 but the player hold aces.
+        if (hand_value > 21 and
+            'A' in [card.split('-')[0] for card in hand]):
+            # If player hold aces, remove 10 points per ace in hand while hand
+            # value is above 21.
+            for card in hand:
+                if card.split('-')[0] == 'A' and hand_value > 21:
+                    hand_value -= 10
+        return hand_value
+
+    def add_card(self):
+        '''Add card to hand'''
+        pass
+
+    def split_hand(self):
+        '''Split hand into two hands.'''
         pass
 
 if __name__ == '__main__':
     # Test code via running it from the main file. Actual game loop
     # will be placed in a Gameplay class.
-    deck = Deck()
-    deck.create_deck()
-    deck.shuffle_deck()
-    print(deck.deck)
-    for card in deck.deck:
-        value, suit = card.split('-')
-        print(f'{value}{deck.suits[suit]}')
+    player = Player()
+    player.hand = ['A-D', '4-H', '3-H', 'A-S']
+    player.get_value()
